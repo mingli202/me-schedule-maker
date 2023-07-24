a3d4c8ba832a61dab900525b529992876696a8cb
Nanoscience202 <vincentmingli@gmail.com>
2023-07-23 23:09:51 -0400

    Julyy 23 2023 (1)

---
+++ scraper/prof.py
@@ -0,0 +1,144 @@
+import json
+from bs4 import BeautifulSoup
+import requests
+
+with open("out.json", "r") as file:
+    arr = json.loads(file.read())
+
+professors = set()
+
+
+for i in arr:
+    professors.add(i["lecture"]["prof"])
+
+    if "prof" in i["lab"]:
+        professors.add(i["lab"]["prof"])
+
+professors.remove("")
+
+with open("professors.json", "w") as file:
+    file.write(json.dumps(list(professors)))
+
+ratings = []
+
+
+def algo(name: str):
+    fName = name.split(",")[0].strip().split(" ")
+    lName = name.split(",")[1].strip().split(" ")
+
+    # name = Giroux, Cynthia Elaine (Cindy)
+    # fName = ["Cynthia", "Elaine", "(Cindy)"]
+    # lName = ["Giroux"]
+
+    allCombinasion = [name]
+
+    # first check will the full name
+    # if can't fund it, search for a variant of the name
+    # finish with first name and last name alone
+
+    for i in fName:
+        for j in lName:
+            allCombinasion.append(i + " " + j)
+
+    allCombinasion += [name.split(",")[0], name.split(",")[1]]
+
+    return allCombinasion
+
+
+for i in professors:
+    pArr = algo(i)
+    print(pArr)
+    length = len(pArr)
+
+    score = 0
+    avg = 0
+    nRating = 0
+    takeAgain = 0
+    difficulty = 0
+    name = ""
+    status = ""
+
+
+    # try for every combinasion of their names
+    for comb in pArr:
+        p = "%20".join(comb.replace(",", "").split(" "))
+
+        url = f"https://www.ratemyprofessors.com/search/professors/12050?q={p}"
+        r = requests.get(url)
+
+        if r.status_code != 200:
+            raise
+
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        script = soup.find_all("script")[-2]
+
+        text = script.text.split(",")
+
+        try:
+            # made sure the teacher is from john abbott
+            # even though in the link it was already specified (12050 is for john abbott)
+            if not '"school":{"__ref":"U2Nob29sLTEyMDUw"}' in text:
+                raise
+
+            name = list(filter(lambda i: "firstName" in i or "lastName" in i, text))
+
+            firstName = name[0].split(":")[1].replace('"', "")
+            lastName = name[1].split(":")[1].replace('"', "")
+
+            # made sure the teacher is the right one
+            # since there could be multiple teacher with the same first or last name
+            if firstName not in i or lastName not in i:
+                raise
+
+            avg = float(list(filter(lambda i: "avgRating" in i, text))[0].split(":")[1])
+            nRating = float(
+                list(filter(lambda i: "numRatings" in i, text))[0].split(":")[1]
+            )
+            takeAgain = float(
+                list(filter(lambda i: "wouldTakeAgainPercent" in i, text))[0].split(
+                    ":"
+                )[1]
+            )
+            difficulty = float(
+                list(filter(lambda i: "avgDifficulty" in i, text))[0].split(":")[1]
+            )
+
+            status = "found"
+
+            break
+
+        except:
+            # continue to next iteration (variant of the name)
+            status = "foundn't"
+
+    # score only for those that exist on the website
+    if avg != 0 and nRating != 0:
+        '''
+        Calculated to take into account the number of raters.
+        You add one 5/5 and one 0/5 to the rating to obtain the score.
+        If there are more raters, the 0/5 will have a lesser impact.
+        
+        e.g. 
+          Gregory Muclair has 5/5 rating but only 2 raters. His score is 75.0
+          Kazuo Takei, Luiz has 4.6 rating with 11 raters. His score is 85.5
+
+        More raters makes the rating stronger and accurate.
+        '''
+        score = round((((avg * nRating) + 5) / (nRating + 2)) * 100 / 5, 1)
+
+    ratings.append(
+        {
+            "prof": i,
+            "score": score,
+            "avg": avg,
+            "nRating": nRating,
+            "takeAgain": takeAgain,
+            "difficulty": difficulty,
+            "status": status,
+        }
+    )
+
+
+with open("ratings.json", "w") as file:
+    file.write(json.dumps(ratings, indent=2))
---
+++ scraper/scraper.py
@@ -0,0 +1,264 @@
+import json
+import re
+
+with open("SCHEDULE_OF_CLASSES_FALL_2023_June_1.txt", "rb") as file:
+    lines = file.read()
+
+
+def writeToA():
+    a = lines.decode("UTF-16")
+    b = a.split("\n")
+
+    def fn(s: str):
+        if (
+            s.strip() == ""
+            or s.strip() == "SCHEDULE OF CLASSES - FALL 2023"
+            or re.match("^John Abbott College", s)
+            or re.match("^SECTION", s)
+        ):
+            return False
+        return True
+
+    raw = list(filter(fn, b))
+
+    with open("a.json", "w") as file:
+        file.write(json.dumps(raw, indent=2))
+
+
+def writeToOut():
+    with open("a.json", "r") as file:
+        raw = json.loads(file.read())
+
+    program = ""
+    course = ""
+    codeHeader = ""
+    code = ""
+    section = ""
+    more = ""
+    disc = ""
+    lecture = {}
+    lab = {}
+
+    time = ""
+    day = ""
+
+    Sections = []
+
+    codeHeader2nLine = False
+
+    codeFormat = (
+        "[0-9][0-9][0-9]-[0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z]-[0-9a-zA-Z][0-9a-zA-Z]"
+    )
+    sectionFormat = "[0-9][0-9][0-9][0-9][0-9]"
+    dayFormat = "^[A-Z][A-Z]?[A-Z]?"
+    timeFormat = "[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]"
+
+    def handleSectionChange(
+        section, disc, lecture, lab, more, code, program, course, codeHeader, count
+    ):
+        Sections.append(
+            {
+                "count": count,
+                "program": program,
+                "course": course,
+                "code": code,
+                "codeHeader": codeHeader,
+                "section": section,
+                "disc": disc,
+                "lecture": lecture,
+                "lab": lab,
+                "more": more,
+            }
+        )
+
+    count = 0
+
+    for index, i in enumerate(raw):
+        # print(index)
+
+        space = len(i) - len(i.lstrip())
+        text = i.strip()
+        a = list(filter(lambda i: i != "", i.split(" ")))
+
+
+        # program
+        if space >= 30 and not any([re.match(timeFormat, j) for j in a]):
+            if text != program:
+                handleSectionChange(
+                    section,
+                    disc,
+                    lecture,
+                    lab,
+                    more,
+                    code,
+                    program,
+                    course,
+                    codeHeader,
+                    count,
+                )
+                section = ""
+                disc = ""
+                lecture = {}
+                lab = {}
+                more = ""
+                code = ""
+                count += 1
+                course = ""
+
+            program = text
+
+        # course
+        elif text.isupper() and space == 0:
+            if course != text:
+                handleSectionChange(
+                    section,
+                    disc,
+                    lecture,
+                    lab,
+                    more,
+                    code,
+                    program,
+                    course,
+                    codeHeader,
+                    count,
+                )
+                section = ""
+                disc = ""
+                lecture = {}
+                lab = {}
+                more = ""
+                code = ""
+                count += 1
+
+            course = text
+
+        # codeheader
+        if space == 1:
+            if codeHeader2nLine:
+                codeHeader += f"\n{text}"
+            else:
+                if text not in codeHeader:
+                    handleSectionChange(
+                        section,
+                        disc,
+                        lecture,
+                        lab,
+                        more,
+                        code,
+                        program,
+                        course,
+                        codeHeader,
+                        count,
+                    )
+                    section = ""
+                    disc = ""
+                    lecture = {}
+                    lab = {}
+                    more = ""
+                    code = ""
+                    count += 1
+
+                codeHeader = text
+
+            codeHeader2nLine = True
+        else:
+            codeHeader2nLine = False
+
+        # section line
+        if re.match(sectionFormat, text):
+            if text != a[0]:
+                handleSectionChange(
+                    section,
+                    disc,
+                    lecture,
+                    lab,
+                    more,
+                    code,
+                    program,
+                    course,
+                    codeHeader,
+                    count,
+                )
+                section = ""
+                disc = ""
+                lecture = {}
+                lab = {}
+                more = ""
+                code = ""
+                count += 1
+
+            section, disc, code, *title, day, time = a
+            lecture = {"title": " ".join(title), day: time}
+
+        # lect line
+        elif re.match("^Lecture", text):
+            if any([re.match(timeFormat, j) for j in a]):
+                l, *prof, day, time = a
+
+            else:
+                l, *prof = a
+
+            lecture.update({"prof": " ".join(prof), day: time})
+
+
+            
+        # lab line
+        elif space == 13:
+            disc, code, *title, day, time = a
+            lab = {"title": " ".join(title), day: time}
+
+        # lab
+        elif re.match("^Laboratory", text):
+            l, *prof = a
+            lab.update({"prof": " ".join(prof)})
+
+        else:
+            if any([re.match(timeFormat, j) for j in a]):
+                *l, day, time = a
+                lecture.update({day: time})
+            elif space in [25, 26]:
+                if re.match("^ADDITIONAL", text):
+                    more += f"{text}\n"
+                else:
+                    more += text
+
+    handleSectionChange(
+        section, disc, lecture, lab, more, code, program, course, codeHeader, count
+    )
+
+    def cond(obj):
+        if obj["section"] == "":
+            return False
+        return True
+
+    filtered = list(filter(cond, Sections))
+
+    with open("out.json", "w") as file:
+        file.write(json.dumps(filtered, indent=2))
+
+    data = {}
+
+    for i in filtered:
+        if i["program"] not in data:
+            data[i["program"]] = {}
+
+        if i["course"] not in data[i["program"]]:
+            data[i["program"]][i["course"]] = {}
+
+        if i["code"] not in data[i["program"]][i["course"]]:
+            data[i["program"]][i["course"]][i["code"]] = {}
+
+        data[i["program"]][i["course"]][i["code"]][i["section"]] = {
+            "disc": i["disc"],
+            "lecture": i["lecture"],
+            "lab": i["lab"],
+            "more": i["more"],
+        }
+
+    with open("data.json", "w") as file:
+        file.write(json.dumps(data, indent=2))
+
+
+if __name__ == "__main__":
+    # // writeToA()
+    writeToOut()
