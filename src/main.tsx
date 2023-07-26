b91c278d3e53c6883ae9572018c1320067617671
Nanoscience202 <vincentmingli@gmail.com>
2023-07-26 17:20:09 -0400

    July 26 2023 (5)

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import Error from "./Error";
 import "./index.css";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Schedule } from "./components";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <Error />,
-      children: [
-        {
-          path:"/",
-          element: <Schedule />
-        }
-      ],
-    },
-  ]
-  // { basename: "/Home/" }
-);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
